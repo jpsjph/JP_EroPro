8c176329e8d7df9acf6973f1754b446a8d2e036f
jpsjph <jpsjph@gmail.com>
2014-09-15 23:03:55 +0100

    Add missing Map Entities

---
+++ Domain.Model/Mapping/ContactMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model.Mapping
+{
+    public class ContactMap : EntityTypeConfiguration<Contact>
+    {
+        public ContactMap()
+        {
+            this.Property(p => p.CreatorId).IsRequired();
+            this.Property(p => p.DateCreated).IsRequired();
+            this.Property(p => p.UpdatorId).IsOptional();
+            this.Property(p => p.DateUpdated).IsOptional();
+        }
+    }
+}
---
+++ Domain.Model/Mapping/TransactionPaymentMap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model.Mapping
+{
+    public class TransactionPaymentMap : EntityTypeConfiguration<TransactionPayment>
+    {
+        public TransactionPaymentMap()
+        {
+            //this.HasKey(k => k.TransactionId);
+            this.Property(p => p.Amount).IsRequired();
+            this.Property(p => p.TransactionDate).IsRequired();         
+            this.Property(p => p.Reference).IsRequired().HasMaxLength(50);
+            this.Property(p => p.OpeningAmount).IsRequired();
+            this.Property(p => p.DueDate).IsRequired();
+
+            this.Property(p => p.CreatorId).IsRequired();
+            this.Property(p => p.DateCreated).IsRequired();
+            this.Property(p => p.UpdatorId).IsOptional();
+            this.Property(p => p.DateUpdated).IsOptional();
+            this.ToTable("Transactions");
+        }
+    }
+}
